1ad61a2a28d0cbfb0f963bc06dcc45f2bcd29436
dtothefp <dtothefp@gmail.com>
2021-12-01 18:26:53 -0800

    chore: parse regex for all component names

--- scripts/verify-soft-tag/verify-soft-tag.ts
+++ scripts/verify-soft-tag/verify-soft-tag.ts
@@ -1,5 +1,5 @@
 import { execSync } from 'child_process';
-import path from 'path';
+import { join } from 'path';
 import { readFileSync } from 'fs';
 import { parse } from 'jsonc-parser';
 import { cyan } from 'chalk';
@@ -9,41 +9,42 @@ type BitMapComponent = {
 };
 type BitMap = Record<string, BitMapComponent>;
 
+// TODO: make this regex more robust
+const replaceScopeAndSemver = (name: string): string => name.replace(/^.+?\/(.+?)@.+$/, '$1');
+
 export function verifySoftTag() {
   const output = execSync('bit status --json', { cwd: process.cwd() });
   const { newComponents, softTaggedComponents, modifiedComponent } = JSON.parse(
-    output.toString()
+    output.toString(),
   );
 
   // enforce that new components have a version and message
-  const newComponentNames = newComponents.map(({ name }) => name);
+  const newComponentNames = newComponents.map(({ name }) => replaceScopeAndSemver(name));
 
   // enforce that non-new modified components have a version and message
-  const modifiedComponentNames = modifiedComponent.map((component) =>
-    component.replace(/^.+?\/(.+?)@.+$/, '$1')
-  );
+  const modifiedComponentNames = modifiedComponent.map(replaceScopeAndSemver);
+  const softTaggedComponentNames = softTaggedComponents.map(replaceScopeAndSemver);
 
-  const fp = path.join(process.cwd(), '.bitmap');
+  const fp = join(process.cwd(), '.bitmap');
   const content = readFileSync(fp, 'utf8');
   const bitmap = parse(content) as BitMap;
 
   return Object.entries(bitmap).reduce((list, [key, value]) => {
     const message = value?.nextVersion?.message;
-    const isMessageMissing =
-      typeof message !== 'string' || message.length === 0;
+    const isMessageMissing = typeof message !== 'string' || message.length === 0;
 
     if (isMessageMissing) {
-      if (softTaggedComponents.includes(key)) {
+      if (softTaggedComponentNames.includes(key)) {
         list.push(
-          `a --message is required for the --soft tagged component ${cyan(key)}`
+          `a --message is required for the --soft tagged component ${cyan(key)}`,
         );
       } else if (newComponentNames.includes(key)) {
         list.push(
-          `add a --soft tag with --message for new component ${cyan(key)}`
+          `add a --soft tag with --message for new component ${cyan(key)}`,
         );
       } else if (modifiedComponentNames.includes(key)) {
         list.push(
-          `add a --soft tag with --message for modified component ${cyan(key)}`
+          `add a --soft tag with --message for modified component ${cyan(key)}`,
         );
       }
     }
